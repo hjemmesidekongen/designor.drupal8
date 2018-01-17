459c7853709554308a3c19f344e2bb13dd9ae717
Morten Nissen <mail@hjemmesidekongen.dk>
2018-01-17 17:12:03 +0100

    Update to v.1

--- docroot/themes/custom/custom_theme/src/javascripts/app.js
+++ docroot/themes/custom/custom_theme/src/javascripts/app.js
@@ -1,27 +1,6 @@
 jQuery(function($) {
     'use strict';
 
-    // Flexy header
-    flexy_header.init();
-
-    // Flexy navigation
-    flexy_navigation.init();
-
-    // Sidr
-    $('.sidr-right__toggle').sidr({
-        name: 'sidr-main',
-        side: 'right',
-        renaming: false,
-        body: '.layout__wrapper',
-        source: '.sidr-source-provider'
-    });
-
-    // Slinky
-    $('.sidr .slinky-menu').slinky({
-        title: true,
-        label: ''
-    });
-
     // Enable / disable Bootstrap tooltips, based upon touch events
     if(Modernizr.touchevents) {
         $('[data-toggle=tooltip]').tooltip('hide');
